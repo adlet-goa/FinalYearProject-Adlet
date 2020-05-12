b2fc005fcfa2b1d0646b67557144c531e595424b
Amey-Volvoikar <volvoikar2639@gmail.com>
2020-05-12 14:46:05 +0530

    Added welcome page after login.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8226,7 +8226,7 @@ var login = /*#__PURE__*/function () {
             if (res.data.status === 'success') {
               (0, _alerts.showAlert)('success', 'Logged in successfully!');
               window.setTimeout(function () {
-                location.assign('/');
+                location.assign('/me');
               }, 1500);
             }
 
@@ -8801,7 +8801,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52031" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52449" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
