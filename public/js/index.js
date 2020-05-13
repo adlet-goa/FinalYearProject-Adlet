ef03964da2b79c78146aeb4daa0b84f00eebf294
Amey-Volvoikar <volvoikar2639@gmail.com>
2020-05-13 22:30:33 +0530

    buy kiosk funtional!

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import { login, logout } from './login';
 import { signup } from './signup';
 import { updateSettings } from './updateSettings';
+import { buykiosk } from './buykiosk';
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
@@ -10,6 +11,8 @@ const logOutBtn = document.querySelector('.nav__el--logout');
 const signupForm = document.querySelector('.form--signup');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const buykioskFrom = document.querySelector('.form--kiosk');
+const buykioskFromGeo = document.querySelector('.form--kioskGeo');
 
 // DELEGATION
 
@@ -59,4 +62,44 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+
+if (buykioskFrom)
+  buykioskFrom.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const address = document.getElementById('address').value;
+    const description = document.getElementById('description').value;
+    const operatingHours = document.getElementById('operatingHours').value;
+    const operatingDays = document.getElementById('operatingDays').value;
+    const screenSize = document.getElementById('screenSize').value;
+    buykiosk(
+      name,
+      lat,
+      lon,
+      address,
+      description,
+      operatingHours,
+      operatingDays,
+      screenSize
+    );
+  });
+
+if (buykioskFromGeo)
+  buykioskFromGeo.addEventListener('click', e => {
+    e.preventDefault();
+    if ('geolocation' in navigator) {
+      console.log('geolocation available');
+      navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        console.log(lat, lon);
+        document.getElementById('lat').value = lat;
+        document.getElementById('lon').value = lon;
+      });
+    } else {
+      console.log('geolocation not available');
+    }
   });
